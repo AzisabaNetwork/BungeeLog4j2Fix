b8ea3425c1c3ac712f0f16c5e36207147e8e351b
acrylic-style <me@acrylicstyle.xyz>
2021-12-11 16:19:14 +0900

    lower and upper

--- src/main/java/net/azisaba/bungeeLog4j2Fix/BungeeLog4j2Fix.java
+++ src/main/java/net/azisaba/bungeeLog4j2Fix/BungeeLog4j2Fix.java
@@ -6,118 +6,119 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class BungeeLog4j2Fix extends Plugin {
     @Override
     public void onLoad() {
+        String replace = ".replaceAll(\"\\\\$\\\\{(lower|upper):(.*?)}\", \"$2\").replaceAll(\"(?i)jndi:ldap\", \"\")";
         Util.redefineClass("net.md_5.bungee.protocol.packet.Kick", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertAfter("{ if (message != null) { message = message.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (message != null) { message = message" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertBefore("{ if (message != null) { message = message.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (message != null) { message = message" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.Team", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (name != null) { name = name" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (name != null) { name = name" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.Title", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (text != null) { text = text.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (text != null) { text = text" + replace + "; } }");
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (text != null) { text = text.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (text != null) { text = text" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.Subtitle", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (text != null) { text = text.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (text != null) { text = text" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (text != null) { text = text.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (text != null) { text = text" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.TabCompleteRequest", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (cursor != null) { cursor = cursor.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (cursor != null) { cursor = cursor" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (cursor != null) { cursor = cursor.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (cursor != null) { cursor = cursor" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.ScoreboardScore", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (itemName != null) { itemName = itemName.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (itemName != null) { itemName = itemName" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (itemName != null) { itemName = itemName.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (itemName != null) { itemName = itemName" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.ScoreboardObjective", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); value = value.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (name != null) { name = name" + replace + "; value = value" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); value = value.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (name != null) { name = name" + replace + "; value = value" + replace + "; } }");
             return cc.toBytecode();
         });
         /*
         Util.redefineClass("net.md_5.bungee.protocol.packet.ScoreboardDisplay", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (name != null) { name = name" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (name != null) { name = name.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (name != null) { name = name" + replace + "; } }");
             return cc.toBytecode();
         });
         */
         Util.redefineClass("net.md_5.bungee.protocol.packet.PluginMessage", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (tag != null) { tag = tag.replaceAll(\"(?i)jndi:ldap\", \"\"); if (\"REGISTER\".equals(tag) || \"minecraft:register\".equals(tag)) { data = new String(data, java.nio.charset.StandardCharsets.UTF_8).replaceAll(\"(?i)jndi:ldap\", \"\").getBytes(java.nio.charset.StandardCharsets.UTF_8); } } }");
+                    .insertAfter("{ if (tag != null) { tag = tag" + replace + "; if (\"REGISTER\".equals(tag) || \"minecraft:register\".equals(tag)) { data = new String(data, java.nio.charset.StandardCharsets.UTF_8)" + replace + ".getBytes(java.nio.charset.StandardCharsets.UTF_8); } } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (tag != null) { tag = tag.replaceAll(\"(?i)jndi:ldap\", \"\"); if (\"REGISTER\".equals(tag) || \"minecraft:register\".equals(tag)) { data = new String(data, java.nio.charset.StandardCharsets.UTF_8).replaceAll(\"(?i)jndi:ldap\", \"\").getBytes(java.nio.charset.StandardCharsets.UTF_8); } } }");
+                    .insertBefore("{ if (tag != null) { tag = tag" + replace + "; if (\"REGISTER\".equals(tag) || \"minecraft:register\".equals(tag)) { data = new String(data, java.nio.charset.StandardCharsets.UTF_8)" + replace + ".getBytes(java.nio.charset.StandardCharsets.UTF_8); } } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.LoginSuccess", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (username != null) { username = username.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (username != null) { username = username" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (username != null) { username = username.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (username != null) { username = username" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.LoginRequest", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertAfter("{ if (data != null) { data = data.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (data != null) { data = data" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertBefore("{ if (data != null) { data = data.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (data != null) { data = data" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.LoginPayloadRequest", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertAfter("{ if (channel != null) { channel = channel.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (channel != null) { channel = channel" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertBefore("{ if (channel != null) { channel = channel.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (channel != null) { channel = channel" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.Handshake", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertAfter("{ if (host != null) { host = host.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (host != null) { host = host" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;)V")
-                    .insertBefore("{ if (host != null) { host = host.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (host != null) { host = host" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.EncryptionRequest", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (serverId != null) { serverId = serverId.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (serverId != null) { serverId = serverId" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (serverId != null) { serverId = serverId.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (serverId != null) { serverId = serverId" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.ClientSettings", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (locale != null) { locale = locale.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (locale != null) { locale = locale" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (locale != null) { locale = locale.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (locale != null) { locale = locale" + replace + "; } }");
             return cc.toBytecode();
         });
         Util.redefineClass("net.md_5.bungee.protocol.packet.Chat", cc -> {
             cc.getMethod("read", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertAfter("{ if (message != null) { message = message.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertAfter("{ if (message != null) { message = message" + replace + "; } }");
             cc.getMethod("write", "(Lio/netty/buffer/ByteBuf;Lnet/md_5/bungee/protocol/ProtocolConstants$Direction;I)V")
-                    .insertBefore("{ if (message != null) { message = message.replaceAll(\"(?i)jndi:ldap\", \"\"); } }");
+                    .insertBefore("{ if (message != null) { message = message" + replace + "; } }");
             return cc.toBytecode();
         });
     }
